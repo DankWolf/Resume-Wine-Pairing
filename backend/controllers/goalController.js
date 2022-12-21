78a4bcd76159682ea14bfaa02cd2af2776d63ee5
Chris Tenorio <christenorio@Chriss-MacBook-Pro.local>
2022-12-21 13:21:28 -0800

    server setup

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,39 @@
+//@desc   Get goals
+//@route  GET /api/goals
+//@access Private
+
+const getGoals = (req, res) => {
+    return res.status(200).json({message: 'Get goals'})
+}
+
+//@desc   set goal
+//@route  Post /api/goals
+//@access Private
+
+const setGoal = (req, res) => {
+    console.log(req.body);
+    return res.status(200).json({message: 'Set goal'})
+}
+
+//@desc   Update goal
+//@route  PUT /api/goals/:id
+//@access Private
+
+const updateGoal = (req, res) => {
+    return res.status(200).json({message: `Update goal ${req.params.id}` })
+}
+
+//@desc   Delete goals
+//@route  DELETE /api/goals/:id
+//@access Private
+
+const deleteGoal = (req, res) => {
+    return res.status(200).json({message: `Delete goal ${req.params.id}` })
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: "Express error handler caught unknown middleware error",
+      status: 400,
+      message: { err: "An error occurred" },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
